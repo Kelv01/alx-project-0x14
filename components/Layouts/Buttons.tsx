1fd05ba1f821bf600e4ed91078f9f215ba68f543
kelv01 <kelvinnjugunanungari@gmail.com>
2025-12-07 21:15:35 +0300

    project setup

--- components/Layouts/Buttons.tsx
+++ components/Layouts/Buttons.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Buttons = () => {
-  return (
-    <div>Buttons</div>
-  )
-}
+  return <div>Buttons</div>;
+};
 
-export default Buttons
+export default Buttons;
